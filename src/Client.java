bedaccbcd388b6705fe2d2fa8b8ed2487d746500
Rafael <saargov97@gmail.com>
2021-05-20 15:09:34 -0300

    Threaded Client

--- src/Client.java
+++ src/Client.java
@@ -3,6 +3,8 @@ import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.imageio.ImageIO;
 import javax.swing.JFrame;
 
@@ -11,17 +13,16 @@ import javax.swing.JFrame;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author rafaelsiebeneichler
  */
-public class Client {
+public class Client implements Runnable {
 
     public JFrame frame;
     public Tela panel;
     public String serverAddr;
-    
+
     public Client(String ip) {
         this.serverAddr = ip;
         this.frame = new JFrame();
@@ -29,25 +30,36 @@ public class Client {
         this.frame.setResizable(true);
         this.frame.add(panel);
         this.frame.pack();
+        //  this.frame.setExtendedState(JFrame.MAXIMIZED_BOTH); 
+        //   this.frame.setUndecorated(true);
         this.frame.setVisible(true);
-        GetTela();
-    }     
-    
-    public void GetTela(){
-       try {
-            BufferedImage image;
-            Socket socket = new Socket(serverAddr, 8080);
-            
-            ObjectInputStream inputStream = new ObjectInputStream(socket.getInputStream());
-            image = ImageIO.read(inputStream);
-            panel.setImg(image);
-            panel.repaint();
-            frame.setVisible(true);
-            
-            socket.close();
-        } catch (IOException e) {
-            e.printStackTrace();
+
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            try {
+                BufferedImage image;
+                Socket socket = new Socket(serverAddr, 8080);
+
+                ObjectInputStream inputStream = new ObjectInputStream(socket.getInputStream());
+                image = ImageIO.read(inputStream);
+                panel.setImg(image);
+                panel.repaint();
+                frame.pack();
+
+                inputStream.close();
+                socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                Thread.sleep(200);
+            } catch (InterruptedException ex) {
+                Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+            }
         }
     }
-    
+
 }
--- src/fmeMain.java
+++ src/fmeMain.java
@@ -126,7 +126,7 @@ public class fmeMain extends javax.swing.JFrame {
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         Client c = new Client(jTxtConnectIP.getText());
-        //
+        new Thread(c).start();
     }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
