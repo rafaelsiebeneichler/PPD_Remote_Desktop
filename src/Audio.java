cc8250412232521e2decb8982e646cb03e2bb31e
douglasvargas1995 <douglas.vargas@universo.univates.ber>
2021-05-27 19:30:14 -0300

    versão - som teste

---
+++ src/Audio.java
@@ -0,0 +1,46 @@
+
+import java.io.File;
+import java.io.IOException;
+import javax.sound.sampled.AudioFileFormat;
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.DataLine;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.TargetDataLine;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author Douglas
+ */
+public class Audio {
+
+    public static void main(String[] args) throws IOException {
+        final CapturandoSom recorder = new CapturandoSom();
+
+        // creates a new thread that waits for a specified
+        // of time before stopping
+        Thread stopper = new Thread(new Runnable() {
+            public void run() {
+                try {
+                    long RECORD_TIME = 0;
+                    Thread.sleep(RECORD_TIME);
+                } catch (InterruptedException ex) {
+                    ex.printStackTrace();
+                }
+                recorder.finish();
+            }
+        });
+
+        stopper.start();
+
+        // start recording
+        recorder.start();
+
+    }
+}
